7303bdd898d23fdf831c7e52db646050461d9c35
BatuhanYigit <batuhanyigit34@gmail.com>
2023-03-06 18:14:43 +0300

    update

---
+++ main.py
@@ -0,0 +1,90 @@
+from fastapi import FastAPI, Request, Response, status
+from fastapi.responses import JSONResponse
+import psycopg2
+import pandas as pd
+from pydantic import BaseModel
+import hashlib
+import sqlquery
+import string
+import secrets
+import datetime
+import os
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8000/user_table",
+    "http://localhost:3000",
+    "file:///home/batuhan/Documents/GitHub/html_table/index.html",
+    "file:///home/batuhan/Documents/GitHub/html_table/index.html",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+conn = psycopg2.connect(
+    host = os.getenv('POSTGRES_HOST'),
+    database = os.getenv('POSTGRES_DB'),
+    user = os.getenv('POSTGRES_USER'),
+    password = os.getenv('POSTGRES_PASSWORD'),
+)
+
+async def create_table():
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS "users" (
+	id serial PRIMARY KEY,
+	email VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR NOT NULL,
+	role VARCHAR (50) NOT NULL,
+	token VARCHAR,
+	expire_time VARCHAR,
+	register_date VARCHAR
+);  """)
+
+
+    
+create_table()
+
+@app.get("/users")
+async def get_table():
+    data_last = pd.read_sql_query("SELECT * FROM users",conn)
+    print(data_last.to_dict("records"))
+    return JSONResponse(
+        content={
+        "data": data_last.to_dict("records"),
+        "success": True
+        }
+    )
+
+@app.get("/user/{id}/")
+async def get_user(
+    id = int
+):  
+    info = {
+        "id":id
+    }
+    
+    data = pd.read_sql_query(sqlquery.get_user.format(**info),conn)
+    print(data.to_dict("records"))
+    return JSONResponse(
+        content={
+        "data":data.to_dict("records"),
+        "success":True
+        }
+    )
---
+++ sqlquery.py
@@ -0,0 +1,48 @@
+create_table_users = """CREATE TABLE IF NOT EXISTS "users" (
+	id serial PRIMARY KEY,
+	email VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR NOT NULL,
+	role VARCHAR (50) NOT NULL,
+	token VARCHAR,
+	expire_time VARCHAR,
+	register_date VARCHAR
+);  
+"""
+
+create_table_lessons = """
+CREATE TABLE IF NOT EXISTS "lessons_notes" (
+	id serial PRIMARY KEY,
+	email VARCHAR ( 50 ) NOT NULL,
+	lesson VARCHAR NOT NULL,
+	exam_marks VARCHAR NOT NULL,
+	letter_grade VARCHAR NOT NULL,
+);  
+"""
+
+insert_data = """INSERT INTO users (email,password,role,register_date) VALUES ('{email}', '{password}', '{role}','{register_date}')"""
+
+check_login = """SELECT * FROM users WHERE email='{email}' AND password='{password}' """
+
+update_token = """UPDATE users SET token='{token}', expire_time='{expire_time}' WHERE email='{email}'"""
+
+check_token = """SELECT * FROM users WHERE token='{token}' and expire_time >'{now}'"""
+
+create_lesson = """INSERT INTO lessons_notes (email,lesson,exam_marks,letter_grade) VALUES ('{email}', '{lesson}', '{exam_marks}', '{letter_grade}') """
+
+check_lesson = """ SELECT * FROM lessons_notes WHERE email='{email}' """
+
+check_email = """SELECT email FROM users WHERE token ='{token}' """
+
+check_role = """SELECT role FROM users WHERE token = '{token}'"""
+
+update_lesson = """UPDATE lessons_notes SET exam_marks = '{exam_marks}', letter_grade = '{letter_grade}'  WHERE email = '{email}' and lesson='{lesson}'"""
+
+check_register_date = """SELECT register_date FROM users WHERE email='{email}'"""
+
+get_user = """SELECT * FROM users WHERE id = {id}"""
+
+
+
+"""
+
+"""
